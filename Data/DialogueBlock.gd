6593477d447a87290bf94e6054d0c80c3080c5e6
Miguel Landi <miguelleallandi10@gmail.com>
2026-09-22 20:22:34 -0300

    DialogueDatabase
    Criado o dialogue  database, possui um array de string com 2 variaveis, dialogos possuidos e dialogos normais.

---
+++ Data/DialogueBlock.gd
@@ -0,0 +1,4 @@
+class_name DialogueBlock
+extends Resource
+
+@export var dialogues: Array[String]
---
+++ Data/NpcDatabase.gd
@@ -0,0 +1,7 @@
+class_name NpcDatabase
+extends Resource
+
+
+@export var name: Array[String]
+@export var normalDialogue: Array[DialogueBlock]
+@export var possessedDialogue: Array[DialogueBlock]
